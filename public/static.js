21f2c21add12ff6f26e1d6368848f941e6c9d38d
Oscar Valdes <oscar94valdes@gmail.com>
2017-04-21 16:03:34 -0400

    started table sort

--- public/static.js
+++ public/static.js
@@ -1,6 +1,9 @@
 $(function() {
   // Connect to our node/websockets server
-  var socket = io.connect('http://aesl.ces.uga.edu:8080/');
+//  var socket = io.connect('http://aesl.ces.uga.edu:8080/');
+  var socket = io.connect('http://aesl.ces.uga.edu:8080/', {
+    reconnection: false
+});
   var statement,
     primaryKey,
     answer = 0,
@@ -24,6 +27,34 @@ $(function() {
         s += '<tr><td>' + rows.join('<tr><td>').replace(/\|/g, '<td>');
         $('#data').html(s);
 
+        $('#data table').find('th').each(function (key, val) {
+          $(this).click(function(){
+            console.log('it worked');
+            var $th = $(this).closest('th');
+            $th.toggleClass('selected');
+            var isSelected = $th.hasClass('selected');
+            var isInput= $th.hasClass('input');
+            var column = $th.index();
+            var $table = $th.closest('table');
+            var isNum= $table.find('tbody > tr').children('td').eq(column).hasClass('num');
+            var rows = $table.find('tbody > tr').get();
+            rows.sort(function(rowA,rowB) {
+              if (isInput) {
+                var keyA = $(rowA).children('td').eq(column).children('input').val().toUpperCase();
+                var keyB = $(rowB).children('td').eq(column).children('input').val().toUpperCase();
+              } else {
+                var keyA = $(rowA).children('td').eq(column).text().toUpperCase();
+                var keyB = $(rowB).children('td').eq(column).text().toUpperCase();
+              }
+              if (isSelected) return OrderBy(keyA,keyB,isNum);
+              return OrderBy(keyB,keyA,isNum);
+});
+  $.each(rows, function(index,row) {
+    $table.children('tbody').append(row);
+  });
+return false;
+          });
+      });
       }) //create table text
   }
 
@@ -67,6 +98,13 @@ $(function() {
     socket.on('create table table', function(data) {
         $('#data').html(data);
       }) //create table table
+  }
+
+  function OrderBy(a,b,n) {
+      if (n) return a-b;
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
   }
 
   $('#edit').hide();
