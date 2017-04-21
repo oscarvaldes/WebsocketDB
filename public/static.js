b765ab0cc547a8b1a5897e9cfad0a5c20837cde8
Oscar Valdes <oscar94valdes@gmail.com>
2017-04-21 16:57:48 -0400

    still working on table sort

--- public/static.js
+++ public/static.js
@@ -48,11 +48,11 @@ $(function() {
               }
               if (isSelected) return OrderBy(keyA,keyB,isNum);
               return OrderBy(keyB,keyA,isNum);
-});
-  $.each(rows, function(index,row) {
-    $table.children('tbody').append(row);
-  });
-return false;
+            });
+              $.each(rows, function(index,row) {
+                $table.children('tbody').append(row);
+              });
+              return false;
           });
       });
       }) //create table text
@@ -83,6 +83,35 @@ return false;
 
         s += '</table>';
         $('#data').html(s);
+
+        $('#data table').find('th').each(function (key, val) {
+          $(this).click(function(){
+            console.log('it worked');
+            var $th = $(this).closest('th');
+            $th.toggleClass('selected');
+            var isSelected = $th.hasClass('selected');
+            var isInput= $th.hasClass('input');
+            var column = $th.index();
+            var $table = $th.closest('table');
+            var isNum= $table.find('tbody > tr').children('td').eq(column).hasClass('num');
+            var rows = $table.find('tbody > tr').get();
+            rows.sort(function(rowA,rowB) {
+              if (isInput) {
+                var keyA = $(rowA).children('td').eq(column).children('input').val().toUpperCase();
+                var keyB = $(rowB).children('td').eq(column).children('input').val().toUpperCase();
+              } else {
+                var keyA = $(rowA).children('td').eq(column).text().toUpperCase();
+                var keyB = $(rowB).children('td').eq(column).text().toUpperCase();
+              }
+              if (isSelected) return OrderBy(keyA,keyB,isNum);
+              return OrderBy(keyB,keyA,isNum);
+            });
+              $.each(rows, function(index,row) {
+                $table.children('tbody').append(row);
+              });
+              return false;
+          });
+      });
 
       }) //create table JSON
 
